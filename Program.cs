038000b65931ddd684ee73262990fbc5e11ac5ad
JupperTV <fmmartyn10@gmail.com>
2023-06-01 22:36:45 +0200

    Translation to English and DRY

--- Program.cs
+++ Program.cs
@@ -1,32 +1,26 @@
-﻿using Microsoft.VisualBasic;
-using System.ComponentModel.DataAnnotations;
-using System.Diagnostics;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System;
+using System.IO;
+using System.Threading;
 
 namespace Program
 {
     class Program
     {
-        // Das ist der String, der gezeigt wird, wenn h eingegeben wird
-        private const string HILFE_SEITE =
+        private const string HELP_PAGE =
 """
 
-  a, append    Binary Hinzufügen
-  b, bin_set   Pfad zum bin-Ordner ändern
-  d, delete    Binary Löschen
-  h, hilfe     Hilfe
-  q, quit      Schließen
+  a, append    Add binary
+  b, bin_set   Change the path of the folder for the binaries
+  d, delete    Delete binary
+  h, hilfe     This help page
+  q, quit      Exit
 
 """;
 
         private const string ENVIRONMENT_VARIABLE_NAME = "BinAppender_BinPath";
         private static string BinPath = string.Empty;
 
-        private static void WaitBeforeBadThingsHappen()  // Hierdurch hat der Nutzer Zeit Ctrl+C zu drücken
+        private static void WaitBeforeBadThingsHappen()  // The user has time to Ctrl+C just in case accidents happen
             => Thread.Sleep(3000);
 
         private static bool StringIsBad(string? inputString)
@@ -34,12 +28,12 @@ namespace Program
 
 
         private static void UserInputIsHelp()
-            => p.cw(HILFE_SEITE);
+            => Console.WriteLine(HELP_PAGE);
 
 
         private static void QuitProgram(int exitCode)
         {
-            p.cw("Programm wird beendet");
+            Console.WriteLine("Program is exiting...");
             Environment.Exit(exitCode);
         }
 
@@ -47,58 +41,56 @@ namespace Program
         private static void UserInputIsDeleteBinary()
         {
             string? binName = null;
-            p.w("\n");
+            Console.Write("\n");
             while (StringIsBad(binName)) {
-                p.w("Gebe den Namen des Binaries, ohne Dateiendung, ein, der gelöscht werden soll:\n-> ");
+                Console.Write("Enter the name of the binary without the file extension:\n-> ");
                 binName = Console.ReadLine();
 
                 if (!File.Exists($"{BinPath}\\{binName}.bat")){
-                    p.cw($"\nDas binary `{binName}` existiert nicht.");
+                    Console.WriteLine($"\nThe binary `{binName}` doesn't exist");
                     binName = string.Empty;
                 }
             }
 
-            p.w($"\nBinary `{binName}` wirklich löschen? (j/n) Wenn etwas anderes als `j` angegeben wird, wird abgebrochen\n-> ");
-            if (Console.ReadLine().ToLower() == "j") {
+            Console.Write($"\nDelete binary `{binName}`? (y/n) Wenn anything else than `y` is entered, the process will be canceled\n-> ");
+            if (Console.ReadLine().ToLower() == "y") {
                 string pathToBinary = $"{BinPath}\\{binName}.bat";
 
                 WaitBeforeBadThingsHappen();
 
                 File.Delete(pathToBinary);
-                p.cw($"Binary {pathToBinary} wurde gelöscht");
-                p.cw("Erledigt!");
+                Console.WriteLine($"Binary {pathToBinary} has been deleted");
+                Console.WriteLine("DONE!");
             } else {
-                p.cw("Vorgang wird abgebrochen");
+                Console.WriteLine("Process canceled");
             }
         }
 
 
-        #region Zum bin-Ordner Schreiben
+        #region Write to bin folder
         /// <summary>
-        /// Name der Batchdatei = Name der .exe Datei
+        /// Name of the batch file = Name of the .exe file
         /// </summary>
         /// <param name="originalFilePath"></param>
-        /// <exception cref="FileNotFoundException"></exception>
-        /// <exception cref="Exception"></exception>
+        /// <exception cref="FileNotFoundException">Path to the original File has not been found</exception>
         private static void WriteBatchToBin(string originalFilePath)
         {
             string batchFilePath = $"{BinPath}\\{Path.GetFileNameWithoutExtension(originalFilePath)}.bat";
 
             if (!File.Exists(originalFilePath))
-                throw new FileNotFoundException($"Die eingegebene Datei `{originalFilePath}`wurde nicht gefunden");
+                throw new FileNotFoundException($"The file `{originalFilePath}` has not been found");
 
             string contentOfBatchFile = $"@echo off\n\"{originalFilePath}\" %*";
             File.WriteAllText(batchFilePath, contentOfBatchFile);
         }
 
         /// <summary>
-        /// Name der Batchdatei = aliasForBatchFile
+        /// Name of the batch file = aliasForBatchFile
         /// </summary>
         /// <param name="originalFilePath"></param>
         /// <param name="aliasForBatchFile">Der andere Name für die Batchdatei (!OHNE DATEIENDUNG!)</param>
-        /// <exception cref="FileNotFoundException">Datei in <paramref name="originalFilePath"/>existiert nicht</exception>
-        /// <exception cref="Exception">Batchdatei mit dem Namen<paramref name="aliasForBatchFile"/>existiert bereits</exception>
-        /// <exception cref="ArgumentNullException"><paramref name="aliasForBatchFile"/>ist Null</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="aliasForBatchFile"/> is null</exception>
+        /// <exception cref="FileNotFoundException">`File in <paramref name="originalFilePath"/> doesn't exist/exception>
         private static void WriteBatchToBin(string originalFilePath, string? aliasForBatchFile)
         {
             if (StringIsBad(aliasForBatchFile))
@@ -120,18 +112,18 @@ namespace Program
             {
                 ConsoleColor originalTextColor = Console.ForegroundColor;
                 Console.ForegroundColor = ConsoleColor.Red;
-                p.cw("Eine Datei mit demselben Namen existert bereits im bin Ordner");
+                Console.WriteLine("A binary with the same name already exists");
                 Console.ForegroundColor = originalTextColor;
 
-                p.w("Datei (u)eberschreiben, mit einem anderen Alias (f)ortfahren, oder Vorgang a(b)brechen? (Default ist abbrechen)\n-> ");
+                Console.Write("(o)verwrite the file, (c)ontinue with a different alias, or cancel program (x)? (Default is cancel program)\n-> ");
 
                 switch (Console.ReadLine().ToLower())
                 {
-                    case "u":
-                        p.w("Datei wirklich überschreiben? (j/n): (Default ist nein und der Vorgang abgebrochen)\n-> ");
-                        if (Console.ReadLine().ToLower() == "j")
+                    case "o":
+                        Console.Write("Really overwrite the file? (y/n): (Default is no and the process will be canceled)\n-> ");
+                        if (Console.ReadLine().ToLower() == "y")
                         {
-                            p.cw($"Der Binary `{batchFilePath}` wird überschrieben");
+                            Console.WriteLine($"The binary `{batchFilePath}` gets overwriten");
                             WaitBeforeBadThingsHappen();
                             return batchFilePath;
                         } else { 
@@ -139,11 +131,11 @@ namespace Program
                         }
                         
 
-                    case "f":
+                    case "c":
                         string? aliasInput = null;
-                        p.w("\n");
+                        Console.Write("\n");
                         while (StringIsBad(aliasInput)){
-                            p.w("Alias ohne Dateiendung eingeben:\n-> ");
+                            Console.Write("Enter alias without the file extension:\n-> ");
                             aliasInput = Console.ReadLine();
                         }
 
@@ -157,61 +149,56 @@ namespace Program
                     default:
                         return string.Empty;  // Es soll abgebrochen werden
                 }
-                batchFilePath = $"{BinPath}\\{Path.GetFileNameWithoutExtension(pathOfOriginalFile)}.bat";
+                batchFilePath = $"{BinPath}\\{Path.GetFileNameWithoutExtension(pathOfOriginalFile)}.bat";  // This code gets executed when a different alias is being entered and the file exists
             }
-            throw new FileNotFoundException("Die Datei wurde nicht gefunden");
+            throw new FileNotFoundException("The file has not been found");
         }
 
 
-        private static void UserWantsAlias(string pathOfOriginalFile, string? aliasForBatchFile, bool ueberschreiben)
+        private static void UserWantsAlias(string pathOfOriginalFile, string? aliasForBatchFile, bool overwrite)
         {
             string batchFilePath = $"{BinPath}\\{Path.GetFileNameWithoutExtension(aliasForBatchFile)}.bat";
             if (File.Exists(batchFilePath))
             {
-                if (ueberschreiben)
-                    WriteBatchToBin(pathOfOriginalFile, aliasForBatchFile);
-
-                if (!ueberschreiben)
+                if (!overwrite)
                 {
                     string? returnedBinPath = WhenBatchFileAlreadyExists(pathOfOriginalFile, batchFilePath);
-                    if (returnedBinPath == string.Empty) {  // Wenn string.Empty wiedergegeben wird, dann will der Nutzer das Programm abbrechen
-                        p.cw("Vorgang wird abgebrochen");
+                    // If string.Empty is returned, then the user wants to cancel the process
+                    if (returnedBinPath == string.Empty) {
+                        Console.WriteLine("Process canceled");
                         return;
                     }
-                    WriteBatchToBin(pathOfOriginalFile, Path.GetFileNameWithoutExtension(returnedBinPath));
+                    aliasForBatchFile = returnedBinPath;
                 }
-
-                p.cw("Erledigt!");
-                return;
             }
 
-            WriteBatchToBin(pathOfOriginalFile, aliasForBatchFile);  // Es it normal, wenn die Datei nicht existiert
-            p.cw("Erledigt!");
+            WriteBatchToBin(pathOfOriginalFile, aliasForBatchFile);
+            Console.WriteLine("Done!");
         }
 
 
-        private static void UserInputIsAppend(bool ueberschreiben)
+        private static void UserInputIsAppend(bool overwrite)
         {
             string? pathOfOriginalFile = null;
-            p.w("\n");  // Die neue Zeile wird erst hier geschrieben, weil der string, vor dem Nutzerinput, ohne 2 Zeilenumbruche ausgegeben werden sikk
+            Console.Write("\n");  // This newline is only being printed out before the user input because it looks prettier
             while (StringIsBad(pathOfOriginalFile))
             {
-                p.w("Gib den Pfad zum originalen Programm ein (Mit Dateiendung):\n-> ");
+                Console.Write("Enter the path to the original file (with the file extension):\n-> ");
                 pathOfOriginalFile = Console.ReadLine();
             }
 
-            // Entferne " vorne und hinten. Windows Explorer kopiert den Pfad mit zwei " vorne und hinten, wenn man es durch `{Dateiname} > Rechts Klick > Als Pfad kopieren` kopiert
+            // Remove any " since Windows Explorer likes to copy the path with 2 " at the beginning and at the end.
+            // Windows Explorer does this when the filepath is copied through {filename} > right-click > Copy as Path
             if (pathOfOriginalFile.Contains('\"'))
                 pathOfOriginalFile = pathOfOriginalFile.Trim('\"');
 
             while (!File.Exists(pathOfOriginalFile))
             {
-                p.w("\nDie Datei existiert nicht. Nochmal eingeben:\n-> ");
+                Console.Write("\nThe file doesn't exist. Enter again:\n-> ");
                 pathOfOriginalFile = Console.ReadLine();
             }
 
-
-            p.w("\nAlias ohne Dateiendung eingeben: (Bei keinem Alias einfach `Enter` drücken)\n-> ");
+            Console.Write("\nEnter alias without a file extension: (Just press enter if there shouldn't be a different name for the binary)\n-> ");
             string? aliasInput = Console.ReadLine();
             if (!StringIsBad(aliasInput))
             {
@@ -221,21 +208,22 @@ namespace Program
 
             string batchFilePath = $"{BinPath}\\{Path.GetFileNameWithoutExtension(pathOfOriginalFile)}.bat";
 
-            if (!ueberschreiben && File.Exists(batchFilePath))
+            if (!overwrite && File.Exists(batchFilePath))
             {
                 string returnedBinPath = WhenBatchFileAlreadyExists(pathOfOriginalFile, batchFilePath);
+                // If string.Empty is returned, then the user wants to cancel the process
                 if (returnedBinPath == string.Empty)
-                {  // Wenn returnedBinPath string.Empty ist, dann wurde abgebrochen
-                    p.cw("Vorgang wird abgebrochen");
+                {
+                    Console.WriteLine("Process cancelled");
                     return;
                 }
 
                 WriteBatchToBin(returnedBinPath);
             }
 
-            p.cw("Es wird kein Alias benutzt");
+            Console.WriteLine("No alias is being used");
             WriteBatchToBin(pathOfOriginalFile);
-            p.cw("Erledigt!");
+            Console.WriteLine("Done!");
         }
         #endregion
 
@@ -243,19 +231,19 @@ namespace Program
         private static void ChangeBinPath()
         {
             string? path = null;
-            p.w("\nPfad zum bin-Ordner eingeben:\n-> ");
+            Console.Write("\nEnter the path to the bin folder:\n-> ");
             while (!Directory.Exists(path) || StringIsBad(path)) {
                 path = Console.ReadLine();
 
                 if (!Directory.Exists(path))
-                    p.cw("Der Ordner existiert nicht. Pfad zum Ordner nochmal eingeben:\n-> ");
+                    Console.WriteLine("The folder doesn't exist. Enter the path again:\n-> ");  // TODO: Add Option to create the folder
                 else if (StringIsBad(path))
-                    p.w("Pfad zum bin-Ordner eingeben:\n-> ");
+                    Console.Write("Enter the path to the bin folder:\n-> ");
             }
             BinPath = path;
             Environment.SetEnvironmentVariable(ENVIRONMENT_VARIABLE_NAME, BinPath, EnvironmentVariableTarget.User);
-            p.cw("Erledigt!");
-            p.cw("\n");
+            Console.WriteLine("Done!");
+            Console.WriteLine("\n");
         }
 
 
@@ -266,20 +254,20 @@ namespace Program
                 BinPath = environmentVariable;
             else
             {
-                p.w("Es wurde noch keine Umgebungsvariable für den bin-Ordner erstellt");
+                Console.Write("No environment variable fo the bin folder has been created");
                 ChangeBinPath();
             }
 
             string? pathVariable = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.User);
             if (StringIsBad(pathVariable)){
-                p.w("Umgebungsvariable \"Path\" wurde nicht gefunden");
+                Console.Write("Environment variable \"Path\" has not been found");
                 QuitProgram(1);
                 return;
             }
 
             if (!pathVariable.Split(';').Contains($"%{ENVIRONMENT_VARIABLE_NAME}%"))
             {
-                p.w($"\nUmgebungsvariable %Path% enthält %{ENVIRONMENT_VARIABLE_NAME}% nicht. %{ENVIRONMENT_VARIABLE_NAME}% wird jetzt hinzugefügt\n");
+                Console.Write($"\nEnvironment variable %Path% doesn't contain %{ENVIRONMENT_VARIABLE_NAME}%. %{ENVIRONMENT_VARIABLE_NAME}% is being added\n");
                 pathVariable = string.Join(';', pathVariable.Split(';').Append($"%{ENVIRONMENT_VARIABLE_NAME}%"));
                 Environment.SetEnvironmentVariable("Path", pathVariable, EnvironmentVariableTarget.User);
             }
@@ -289,11 +277,13 @@ namespace Program
         public static void Main()
         {
             InitBinPath();
+
             string? userInput;
+
             while (true)
             {
-                p.w("Was soll gemacht werden? (h für file):\n-> ");
-                if (StringIsBad(userInput = Console.ReadLine()))  // Benutzer hat Enter eingegeben 
+                Console.Write("Enter an option (h for help):\n-> ");
+                if (StringIsBad(userInput = Console.ReadLine())) 
                     continue;
 
                 switch (userInput.ToLower())
@@ -308,14 +298,20 @@ namespace Program
                         ChangeBinPath();
                         break;
 
+                    case "d":
+                    case "delete":
+                        UserInputIsDeleteBinary();
+                        break;
+
                     case "h":
                     case "help":
                         UserInputIsHelp();
                         break;
 
-                    case "d":
-                    case "delete":
-                        UserInputIsDeleteBinary();
+                    case "o":
+                    case "overwrite":
+                        throw new NotImplementedException("TODO: Add Option to overwrite binaries (with a new executable)");
+                        UserInputIsAppend(true);  // It might be this easy, but I haven't tested it yet
                         break;
 
                     case "q":
@@ -326,7 +322,7 @@ namespace Program
                     default: continue;
                 }
 
-                p.cw($"\n{new string('-', Console.WindowWidth)}\n");
+                Console.WriteLine($"\n{new string('-', Console.WindowWidth)}\n");  // After any option has been completed or cancelled
             }
         }
     }
